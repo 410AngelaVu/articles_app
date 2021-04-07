c44c0b36ed8cf184e7dee3c46e3936ec715b5318
410AngelaVu <andjela1993@gmail.com>
2021-04-07 21:30:14 +0200

    Created Article model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
+  resources :articles 
+  root 'homes#index'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
