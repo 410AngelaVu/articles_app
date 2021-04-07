f01455e8c4a000abefd1898eb67aefdeed00c592
410AngelaVu <andjela1993@gmail.com>
2021-04-07 23:21:50 +0200

    Added Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :articles 
+  namespace :author do
+  	resources :articles
+  end
+
+  resources :articles, only: [:index, :show] 
   root 'homes#index'
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
