45eab5171ef1d60c435011ecd75f3edcc5ab6b3d
410AngelaVu <andjela1993@gmail.com>
2021-04-08 18:38:28 +0200

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: 'author',
+  email: 'author122@test.com',
+  password: 'password@1',
+  is_author: true
+)
+
+# Create a Normal user
+User.create!(
+  name: 'user',
+  email: 'user1221@test.com',
+  password: 'password@1',
+  is_author: false
+)
+
+3.times do |i|
+  Article.create!(title: "Hello World#{i}", body: "Little Article #{i}", author_id: 1)
+end
